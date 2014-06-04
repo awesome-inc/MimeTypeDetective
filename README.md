0fc17e80eacebba5c7c38305e4d4f19a43f817a2
tmentzel <thomas.mentzel@schoenhofer.de>
2014-06-04 12:57:31 +0200

    minor fixes to the file extension lookup to have the same result than Urlmon.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 MimeTypeDetective
 =================
 
-inspect file to gather the mime type
+inspect file to gather the mime type.
+
+GetMimeType is an extension method for the `FileInfo` class.
+
+usage:
+
+    [Test()]
+    [TestCase(@"data\Textdokument.txt", "text/plain")]
+    [TestCase(@"data\Notify.wav", "audio/wav")]
+    [TestCase(@"data\Overture.mp3", "audio/mpeg3")]
+    [TestCase(@"data\Unknowndokument.thisisnotanextension", "application/octet-stream")]
+    public void TryFilesWhichWorkWithUrlmon(string filename, string mimetype)
+    {
+        var fi = new FileInfo(filename);
+
+        var mime = fi.GetMimeType();
+
+        mime.Should().Be(mimetype);
+    }
---
+++ Softwarekueche.MimeTypeDetective.Tests/FileExtensionTests.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Softwarekueche.MimeTypeDetective.Tests
+{
+    [TestFixture()]
+    public class FileExtensionTests
+    {
+        [Test()]
+        [TestCase(@"data\Textdokument.txt", "text/plain")]
+        [TestCase(@"data\Notify.wav", "audio/wav")]
+        [TestCase(@"data\Overture.mp3", "audio/mpeg3")]
+        [TestCase(@"data\Unknowndokument.thisisnotanextension", "application/octet-stream")]
+        public void TryFilesWhichWorkWithUrlmon(string filename, string mimetype)
+        {
+            var fi = new FileInfo(filename);
+
+            var mime = fi.GetMimeType();
+
+            mime.Should().Be(mimetype);
+        }
+    }
+}
